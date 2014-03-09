bda40ed9a312299ddef94164905003eb54f93c5f
shaojianhong <shaojianhong@shao.mshome.net>
2014-03-09 23:33:16 +0800

    提交

--- CCC_Source/src/com/ccc/dreamfile/excel/FileExcelSheet.java
+++ CCC_Source/src/com/ccc/dreamfile/excel/FileExcelSheet.java
@@ -7,22 +7,20 @@ import java.util.List;
  * @date 2012-05-06 17:50:28
  */
 public class FileExcelSheet {
-    private List<FileExcelRow> cells;
-    private String sheetName;
+	private final List<FileExcelRow> cells;
+	private final String sheetName;
 
-    public List<FileExcelRow> getRows() {
-        return this.cells;
-    }
+	public FileExcelSheet(String sheetName, List<FileExcelRow> cells) {
+		this.sheetName = sheetName;
+		this.cells = cells;
+	}
 
-    public void setRows(List<FileExcelRow> rows) {
-        this.cells = rows;
-    }
+	public List<FileExcelRow> getRows() {
+		return this.cells;
+	}
 
-    public String getSheetName() {
-        return this.sheetName;
-    }
+	public String getSheetName() {
+		return this.sheetName;
+	}
 
-    public void setSheetName(String sheetName) {
-        this.sheetName = sheetName;
-    }
 }
