da0da748fd1b13b68f180c52417e8d5a0bc30007
shaojianhong <shaojianhong@shao.mshome.net>
2014-03-09 22:33:53 +0800

    提交

--- CCC_Source/src/com/ccc/dreamfile/compress/FileRarUtil.java
+++ CCC_Source/src/com/ccc/dreamfile/compress/FileRarUtil.java
@@ -15,138 +15,138 @@ import com.ccc.dreamtag.tag.function.StringUtil;
 
 public class FileRarUtil {
 
-    private FileRarUtil() {
-    }
+	private FileRarUtil() {
+	}
 
-    /**
-     * unrar rar file,will not unrar contains unrar file. <br>
-     * 
-     * @param cmd：
-     *            like C:\\Program Files\\WinRAR\\winrar.exe
-     * @param rarFileName：rar
-     *            fullpath
-     * @param toPath：if
-     *            toPath is null,then unrar to this path.
-     */
-    public static void unRar(String cmd, String rarFileName, String toPath) {
-        try {
-            if (!rarFileName.endsWith(".rar") && !rarFileName.endsWith(".RAR")) {
-                LogDream.info("file is no rar file");
-                return;
-            }
-            File file = new File(rarFileName);
-            if (!file.exists()) {
-                LogDream.info("file is no exist");
-                return;
-            }
-            if (StringUtil.isEmpty(toPath)) {
-                toPath = rarFileName.replaceAll(".rar", "").replace(".rar", "");
-            }
-            File file2 = new File(toPath);
-            if (!file2.exists()) {
-                file2.mkdir();
-            }
-            String unrarCmd = cmd + " x -r -p- -o+ " + rarFileName + " " + toPath;
-            Runtime rt = Runtime.getRuntime();
-            Process pre = rt.exec(unrarCmd);
-            InputStreamReader isr = new InputStreamReader(pre.getInputStream());
-            BufferedReader bf = new BufferedReader(isr);
-            String line = null;
-            while ((line = bf.readLine()) != null) {
-                line = line.trim();
-                if ("".equals(line)) {
-                    continue;
-                }
-                System.out.println(line);
-            }
-            bf.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * unrar rar file,will not unrar contains unrar file. <br>
+	 * 
+	 * @param cmd
+	 *            ： like C:\\Program Files\\WinRAR\\winrar.exe
+	 * @param rarFileName
+	 *            ：rar fullpath
+	 * @param toPath
+	 *            ：if toPath is null,then unrar to this path.
+	 */
+	public static void unRar(String cmd, String rarFileName, String toPath) {
+		try {
+			if (!rarFileName.endsWith(".rar") && !rarFileName.endsWith(".RAR")) {
+				LogDream.info("file is no rar file");
+				return;
+			}
+			File file = new File(rarFileName);
+			if (!file.exists()) {
+				LogDream.info("file is no exist");
+				return;
+			}
+			if (StringUtil.isEmpty(toPath)) {
+				toPath = rarFileName.replaceAll(".rar", "").replace(".rar", "");
+			}
+			File file2 = new File(toPath);
+			if (!file2.exists()) {
+				file2.mkdir();
+			}
+			String unrarCmd = cmd + " x -r -p- -o+ " + rarFileName + " " + toPath;
+			Runtime rt = Runtime.getRuntime();
+			Process pre = rt.exec(unrarCmd);
+			InputStreamReader isr = new InputStreamReader(pre.getInputStream());
+			BufferedReader bf = new BufferedReader(isr);
+			String line = null;
+			while ((line = bf.readLine()) != null) {
+				line = line.trim();
+				if ("".equals(line)) {
+					continue;
+				}
+				System.out.println(line);
+			}
+			bf.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * unrar rar file,will not unrar contains unrar file. <br>
-     * 
-     * @param cmd：
-     *            like C:\\Program Files\\WinRAR\\winrar.exe
-     * @param rarFileName：rar
-     *            fullpath
-     */
-    public static void unRarPathDept(String cmd, String rarFileName) {
-        try {
-            File file = new File(rarFileName);
-            if (!file.exists() || !file.isDirectory()) {
-                LogDream.info("file is no exist");
-                return;
-            }
-            for (File file2 : file.listFiles()) {
-                if ((file2.getName().endsWith(".rar") || file2.getName().endsWith(".RAR")) && file2.isFile()) {
-                    unRarDept(cmd, file2.getAbsolutePath(), null);
-                } else if (file2.isDirectory()) {
-                    unRarPathDept(cmd, file2.getAbsolutePath());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * unrar rar file,will not unrar contains unrar file. <br>
+	 * 
+	 * @param cmd
+	 *            ： like C:\\Program Files\\WinRAR\\winrar.exe
+	 * @param rarFileName
+	 *            ：rar fullpath
+	 */
+	public static void unRarPathDept(String cmd, String rarFileName) {
+		try {
+			File file = new File(rarFileName);
+			if (!file.exists() || !file.isDirectory()) {
+				LogDream.info("file is no exist");
+				return;
+			}
+			for (File file2 : file.listFiles()) {
+				if ((file2.getName().endsWith(".rar") || file2.getName().endsWith(".RAR")) && file2.isFile()) {
+					unRarDept(cmd, file2.getAbsolutePath(), null);
+				} else if (file2.isDirectory()) {
+					unRarPathDept(cmd, file2.getAbsolutePath());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * unrar rar file,will not unrar contains unrar file. <br>
-     * 
-     * @param cmd：
-     *            like C:\\Program Files\\WinRAR\\winrar.exe
-     * @param rarFileName：rar
-     *            fullpath
-     * @param toPath：if
-     *            toPath is null,then unrar to this path.
-     */
-    public static void unRarDept(String cmd, String rarFileName, String toPath) {
-        try {
-            if (!rarFileName.endsWith(".rar") && !rarFileName.endsWith(".RAR")) {
-                LogDream.info("file is not rar file");
-                return;
-            }
-            File file = new File(rarFileName);
-            if (!file.exists()) {
-                LogDream.info("file is no exist");
-                return;
-            }
-            if (StringUtil.isEmpty(toPath)) {
-                toPath = rarFileName.replaceAll(".rar", "").replace(".RAR", "");
-            }
-            File file2 = new File(toPath);
-            if (!file2.exists()) {
-                file2.mkdir();
-            }
-            String unrarCmd = cmd + " x -r -p- -o+ " + rarFileName + " " + toPath;
-            Runtime rt = Runtime.getRuntime();
-            Process pre = rt.exec(unrarCmd);
-            InputStreamReader isr = new InputStreamReader(pre.getInputStream());
-            BufferedReader bf = new BufferedReader(isr);
-            String line = null;
-            while ((line = bf.readLine()) != null) {
-                line = line.trim();
-                if ("".equals(line)) {
-                    continue;
-                }
-                System.out.println(line);
-            }
-            bf.close();
-            file.delete();
-            File file3 = new File(toPath);
-            for (File file4 : file3.listFiles()) {
-                if ((file4.getName().endsWith(".rar") || file4.getName().endsWith(".RAR")) && file4.isFile()) {
-                    unRarDept(cmd, file4.getAbsolutePath(), null);
-                } else if (file4.isDirectory()) {
-                    unRarPathDept(cmd, file4.getAbsolutePath());
-                }
-            }
+	/**
+	 * unrar rar file,will not unrar contains unrar file. <br>
+	 * 
+	 * @param cmd
+	 *            ： like C:\\Program Files\\WinRAR\\winrar.exe
+	 * @param rarFileName
+	 *            ：rar fullpath
+	 * @param toPath
+	 *            ：if toPath is null,then unrar to this path.
+	 */
+	public static void unRarDept(String cmd, String rarFileName, String toPath) {
+		try {
+			if (!rarFileName.endsWith(".rar") && !rarFileName.endsWith(".RAR")) {
+				LogDream.info("file is not rar file");
+				return;
+			}
+			File file = new File(rarFileName);
+			if (!file.exists()) {
+				LogDream.info("file is no exist");
+				return;
+			}
+			if (StringUtil.isEmpty(toPath)) {
+				toPath = rarFileName.replaceAll(".rar", "").replace(".RAR", "");
+			}
+			File file2 = new File(toPath);
+			if (!file2.exists()) {
+				file2.mkdir();
+			}
+			String unrarCmd = cmd + " x -r -p- -o+ " + rarFileName + " " + toPath;
+			Runtime rt = Runtime.getRuntime();
+			Process pre = rt.exec(unrarCmd);
+			InputStreamReader isr = new InputStreamReader(pre.getInputStream());
+			BufferedReader bf = new BufferedReader(isr);
+			String line = null;
+			while ((line = bf.readLine()) != null) {
+				line = line.trim();
+				if ("".equals(line)) {
+					continue;
+				}
+				System.out.println(line);
+			}
+			bf.close();
+			file.delete();
+			File file3 = new File(toPath);
+			for (File file4 : file3.listFiles()) {
+				if ((file4.getName().endsWith(".rar") || file4.getName().endsWith(".RAR")) && file4.isFile()) {
+					unRarDept(cmd, file4.getAbsolutePath(), null);
+				} else if (file4.isDirectory()) {
+					unRarPathDept(cmd, file4.getAbsolutePath());
+				}
+			}
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
--- CCC_Source/src/com/ccc/dreamfile/compress/FileZipUtil.java
+++ CCC_Source/src/com/ccc/dreamfile/compress/FileZipUtil.java
@@ -22,226 +22,226 @@ import com.ccc.dreamtag.tag.function.StringUtil;
 
 public class FileZipUtil {
 
-    private FileZipUtil() {
-    }
+	private FileZipUtil() {
+	}
 
-    /**
-     * unzip zip file,will not unzip contains zip file. <br>
-     * 
-     * @param zipFileName：zip
-     *            full path
-     * @param toPath:if
-     *            toPath is null,then unzip to this path.
-     */
-    public static void unZip(String zipFileName, String toPath) {
-        File file = new File(zipFileName);
-        if ((!zipFileName.endsWith(".zip") && !zipFileName.endsWith(".ZIP")) || !file.exists()) {
-            LogDream.info("zip file unexist");
-            return;
-        }
-        File outFile;
-        if (StringUtil.isEmpty(toPath)) {
-            toPath = zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", "");
-            outFile = new File(zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", ""));
-            if (!outFile.exists()) {
-                outFile.mkdir();
-            }
-        } else {
-            outFile = new File(toPath);
-            if (!outFile.exists()) {
-                outFile.mkdir();
-            }
-        }
-        try {
-            ZipFile zipFile = new ZipFile(zipFileName);
-            Enumeration<ZipEntry> e = zipFile.getEntries();
-            ZipEntry zipEntry = null;
-            while (e.hasMoreElements()) {
-                zipEntry = e.nextElement();
-                if (zipEntry.isDirectory()) {
-                    String name = zipEntry.getName();
-                    name = name.substring(0, name.length() - 1);
-                    File f = new File(toPath + File.separator + name);
-                    f.mkdir();
-                } else {
-                    String fileName = zipEntry.getName();
-                    fileName = fileName.replace('\\', '/');
-                    if (fileName.indexOf("/") != -1) {
-                        createDirectory(toPath, fileName.substring(0, fileName.lastIndexOf("/")));
-                        fileName = fileName.substring(fileName.lastIndexOf("/") + 1, fileName.length());
-                    }
+	/**
+	 * unzip zip file,will not unzip contains zip file. <br>
+	 * 
+	 * @param zipFileName
+	 *            ：zip full path
+	 * @param toPath
+	 *            :if toPath is null,then unzip to this path.
+	 */
+	public static void unZip(String zipFileName, String toPath) {
+		File file = new File(zipFileName);
+		if ((!zipFileName.endsWith(".zip") && !zipFileName.endsWith(".ZIP")) || !file.exists()) {
+			LogDream.info("zip file unexist");
+			return;
+		}
+		File outFile;
+		if (StringUtil.isEmpty(toPath)) {
+			toPath = zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", "");
+			outFile = new File(zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", ""));
+			if (!outFile.exists()) {
+				outFile.mkdir();
+			}
+		} else {
+			outFile = new File(toPath);
+			if (!outFile.exists()) {
+				outFile.mkdir();
+			}
+		}
+		try {
+			ZipFile zipFile = new ZipFile(zipFileName);
+			Enumeration<ZipEntry> e = zipFile.getEntries();
+			ZipEntry zipEntry = null;
+			while (e.hasMoreElements()) {
+				zipEntry = e.nextElement();
+				if (zipEntry.isDirectory()) {
+					String name = zipEntry.getName();
+					name = name.substring(0, name.length() - 1);
+					File f = new File(toPath + File.separator + name);
+					f.mkdir();
+				} else {
+					String fileName = zipEntry.getName();
+					fileName = fileName.replace('\\', '/');
+					if (fileName.indexOf("/") != -1) {
+						createDirectory(toPath, fileName.substring(0, fileName.lastIndexOf("/")));
+						fileName = fileName.substring(fileName.lastIndexOf("/") + 1, fileName.length());
+					}
 
-                    File f = new File(toPath + File.separator + zipEntry.getName());
-                    f.createNewFile();
-                    InputStream in = zipFile.getInputStream(zipEntry);
-                    FileOutputStream out = new FileOutputStream(f);
-                    byte[] by = new byte[1024];
-                    int c;
-                    while ((c = in.read(by)) != -1) {
-                        out.write(by, 0, c);
-                    }
-                    out.close();
-                    in.close();
-                }
-            }
-            zipFile.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+					File f = new File(toPath + File.separator + zipEntry.getName());
+					f.createNewFile();
+					InputStream in = zipFile.getInputStream(zipEntry);
+					FileOutputStream out = new FileOutputStream(f);
+					byte[] by = new byte[1024];
+					int c;
+					while ((c = in.read(by)) != -1) {
+						out.write(by, 0, c);
+					}
+					out.close();
+					in.close();
+				}
+			}
+			zipFile.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    /**
-     * unzip zip file,will unzip contains zip file. <br>
-     * 
-     * @param zipFileName：zip
-     *            full path
-     * @param toPath:if
-     *            toPath is null,then unzip to this path.
-     */
-    public static void unZipDept(String zipFileName, String toPath) {
-        File file = new File(zipFileName);
-        if (!file.exists() || (!zipFileName.endsWith(".zip") && !zipFileName.endsWith(".ZIP"))) {
-            LogDream.info("zip file unexist");
-            return;
-        }
-        File outFile;
-        if (StringUtil.isEmpty(toPath)) {
-            toPath = zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", "");
-            outFile = new File(zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", ""));
-            if (!outFile.exists()) {
-                outFile.mkdir();
-            }
-        } else {
-            outFile = new File(toPath);
-            if (!outFile.exists()) {
-                outFile.mkdir();
-            }
-        }
-        try {
-            ZipFile zipFile = new ZipFile(zipFileName);
-            Enumeration e = zipFile.getEntries();
-            ZipEntry zipEntry = null;
-            while (e.hasMoreElements()) {
-                zipEntry = (ZipEntry) e.nextElement();
-                if (zipEntry.isDirectory()) {
-                    String name = zipEntry.getName();
-                    name = name.substring(0, name.length() - 1);
-                    File f = new File(toPath + File.separator + name);
-                    f.mkdir();
-                } else {
-                    String fileName = zipEntry.getName();
-                    fileName = fileName.replace('\\', '/');
-                    if (fileName.indexOf("/") != -1) {
-                        createDirectory(toPath, fileName.substring(0, fileName.lastIndexOf("/")));
-                        fileName = fileName.substring(fileName.lastIndexOf("/") + 1, fileName.length());
-                    }
+	/**
+	 * unzip zip file,will unzip contains zip file. <br>
+	 * 
+	 * @param zipFileName
+	 *            ：zip full path
+	 * @param toPath
+	 *            :if toPath is null,then unzip to this path.
+	 */
+	public static void unZipDept(String zipFileName, String toPath) {
+		File file = new File(zipFileName);
+		if (!file.exists() || (!zipFileName.endsWith(".zip") && !zipFileName.endsWith(".ZIP"))) {
+			LogDream.info("zip file unexist");
+			return;
+		}
+		File outFile;
+		if (StringUtil.isEmpty(toPath)) {
+			toPath = zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", "");
+			outFile = new File(zipFileName.replaceAll(".zip", "").replaceAll(".ZIP", ""));
+			if (!outFile.exists()) {
+				outFile.mkdir();
+			}
+		} else {
+			outFile = new File(toPath);
+			if (!outFile.exists()) {
+				outFile.mkdir();
+			}
+		}
+		try {
+			ZipFile zipFile = new ZipFile(zipFileName);
+			Enumeration e = zipFile.getEntries();
+			ZipEntry zipEntry = null;
+			while (e.hasMoreElements()) {
+				zipEntry = (ZipEntry) e.nextElement();
+				if (zipEntry.isDirectory()) {
+					String name = zipEntry.getName();
+					name = name.substring(0, name.length() - 1);
+					File f = new File(toPath + File.separator + name);
+					f.mkdir();
+				} else {
+					String fileName = zipEntry.getName();
+					fileName = fileName.replace('\\', '/');
+					if (fileName.indexOf("/") != -1) {
+						createDirectory(toPath, fileName.substring(0, fileName.lastIndexOf("/")));
+						fileName = fileName.substring(fileName.lastIndexOf("/") + 1, fileName.length());
+					}
 
-                    File f = new File(toPath + File.separator + zipEntry.getName());
-                    f.createNewFile();
-                    InputStream in = zipFile.getInputStream(zipEntry);
-                    FileOutputStream out = new FileOutputStream(f);
-                    byte[] by = new byte[1024];
-                    int c;
-                    while ((c = in.read(by)) != -1) {
-                        out.write(by, 0, c);
-                    }
-                    out.close();
-                    in.close();
-                }
-            }
-            zipFile.close();
-            File file3 = new File(zipFileName);
-            file3.deleteOnExit();
-            for (File file4 : outFile.listFiles()) {
-                if ((file4.getName().endsWith(".zip") || file4.getName().endsWith(".ZIP")) && file4.isFile()) {
-                    unZipDept(file4.getAbsolutePath(), null);
-                } else if (file4.isDirectory()) {
-                    unZipPathDept(file4.getAbsolutePath());
-                }
-            }
+					File f = new File(toPath + File.separator + zipEntry.getName());
+					f.createNewFile();
+					InputStream in = zipFile.getInputStream(zipEntry);
+					FileOutputStream out = new FileOutputStream(f);
+					byte[] by = new byte[1024];
+					int c;
+					while ((c = in.read(by)) != -1) {
+						out.write(by, 0, c);
+					}
+					out.close();
+					in.close();
+				}
+			}
+			zipFile.close();
+			File file3 = new File(zipFileName);
+			file3.deleteOnExit();
+			for (File file4 : outFile.listFiles()) {
+				if ((file4.getName().endsWith(".zip") || file4.getName().endsWith(".ZIP")) && file4.isFile()) {
+					unZipDept(file4.getAbsolutePath(), null);
+				} else if (file4.isDirectory()) {
+					unZipPathDept(file4.getAbsolutePath());
+				}
+			}
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    /**
-     * unzip zip file,will unzip contains zip file. <br>
-     * 
-     * @param zipFileName：zip
-     *            full path
-     * @param toPath:if
-     *            toPath is null,then unzip to this path.
-     */
-    public static void unZipPathDept(String zipFilePath) {
+	/**
+	 * unzip zip file,will unzip contains zip file. <br>
+	 * 
+	 * @param zipFileName
+	 *            ：zip full path
+	 * @param toPath
+	 *            :if toPath is null,then unzip to this path.
+	 */
+	public static void unZipPathDept(String zipFilePath) {
 
-        try {
-            File file = new File(zipFilePath);
-            if (!file.exists() || !file.isDirectory()) {
-                LogDream.info("file is not exist");
-                return;
-            }
-            for (File file2 : file.listFiles()) {
-                if ((file2.getName().endsWith(".zip") || file2.getName().endsWith(".ZIP")) && file2.isFile()) {
-                    unZipDept(file2.getAbsolutePath(), null);
-                } else if (file2.isDirectory()) {
-                    unZipPathDept(file2.getAbsolutePath());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		try {
+			File file = new File(zipFilePath);
+			if (!file.exists() || !file.isDirectory()) {
+				LogDream.info(" file is not exist or file is not a directory");
+				return;
+			}
+			for (File file2 : file.listFiles()) {
+				if ((file2.getName().endsWith(".zip") || file2.getName().endsWith(".ZIP")) && file2.isFile()) {
+					unZipDept(file2.getAbsolutePath(), null);
+				} else if (file2.isDirectory()) {
+					unZipPathDept(file2.getAbsolutePath());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 
-    // create file path
-    private static void createDirectory(String directory, String subDirectory) {
-        String dir[];
-        File fl = new File(directory);
-        try {
-            if ("".equals(subDirectory) && fl.exists() != true)
-                fl.mkdirs();
-            else if (!"".equals(subDirectory)) {
-                dir = subDirectory.replace("\\", "/").split("/");
-                for (int i = 0; i < dir.length; i++) {
-                    File subFile = new File(directory + File.separator + dir[i]);
-                    if (!subFile.exists())
-                        subFile.mkdir();
-                    directory += File.separator + dir[i];
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+	// create file path
+	private static void createDirectory(String directory, String subDirectory) {
+		String dir[];
+		File fl = new File(directory);
+		try {
+			if ("".equals(subDirectory) && fl.exists() != true)
+				fl.mkdirs();
+			else if (!"".equals(subDirectory)) {
+				dir = subDirectory.replace("\\", "/").split("/");
+				for (int i = 0; i < dir.length; i++) {
+					File subFile = new File(directory + File.separator + dir[i]);
+					if (!subFile.exists())
+						subFile.mkdir();
+					directory += File.separator + dir[i];
+				}
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    /**
-     * zip file
-     * 
-     * @param sourceFile
-     * @param targetFile
-     * @return
-     */
-    public static boolean zip(File sourceFile, File targetFile) {
-        try {
-            Project prj = new Project();
-            Zip zip = new Zip();
-            zip.setProject(prj);
-            zip.setDestFile(targetFile);
-            FileSet fileSet = new FileSet();
-            fileSet.setProject(prj);
-            if (sourceFile.isFile()) {
-                fileSet.setFile(sourceFile);
-            } else {
+	/**
+	 * zip file
+	 * 
+	 * @param sourceFile
+	 * @param targetFile
+	 * @return
+	 */
+	public static boolean zip(File sourceFile, File targetFile) {
+		try {
+			Project prj = new Project();
+			Zip zip = new Zip();
+			zip.setProject(prj);
+			zip.setDestFile(targetFile);
+			FileSet fileSet = new FileSet();
+			fileSet.setProject(prj);
+			if (sourceFile.isFile()) {
+				fileSet.setFile(sourceFile);
+			} else {
 
-                fileSet.setDir(sourceFile);
-            }
-            zip.addFileset(fileSet);
-            zip.execute();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+				fileSet.setDir(sourceFile);
+			}
+			zip.addFileset(fileSet);
+			zip.execute();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
